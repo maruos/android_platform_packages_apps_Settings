587b5944c3528d9d67485dbb07e060783b4831dc
Preetam D'Souza <preetamjdsouza@gmail.com>
2016-07-05 18:00:27 -0400

    Add license headers
    (cherry picked from commit 8e2d413ee11f994721f0d24255694e37a7746efa)
    Signed-off-by: Preetam D'Souza <preetamjdsouza@gmail.com>

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -1,6 +1,8 @@
 /*
  * Copyright (C) 2008 The Android Open Source Project
  * Copyright (C) 2016 The CyanogenMod Project
+ * Copyright (C) 2015-2016 Preetam J. D'Souza
+ * Copyright (C) 2016 The Maru OS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
