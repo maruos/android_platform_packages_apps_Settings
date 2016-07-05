892a4969328796d7ab8232c99ce5bc8be0a5ff95
Preetam D'Souza <preetamjdsouza@gmail.com>
2016-07-05 18:00:27 -0400

    Add license headers
    (cherry picked from commit 8e2d413ee11f994721f0d24255694e37a7746efa)
    Signed-off-by: Preetam D'Souza <preetamjdsouza@gmail.com>

--- src/com/android/settings/mdesktop/HdmiDisplayListener.java
+++ src/com/android/settings/mdesktop/HdmiDisplayListener.java
@@ -1,12 +1,26 @@
+/*
+ * Copyright 2015-2016 Preetam J. D'Souza
+ * Copyright 2016 The Maru OS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.android.settings.mdesktop;
 
 import android.content.Context;
 import android.hardware.display.DisplayManager;
 import android.view.Display;
 
-/**
- * Created by pdsouza on 11/10/15.
- */
 public class HdmiDisplayListener implements DisplayManager.DisplayListener {
     private static final String TAG = HdmiDisplayListener.class.getName();
     private final Context mContext;
